e9e214c35bd84609e98d675f625c330ac1cd23f8
jim <jimorcheson@gmail.com>
2023-09-14 07:44:36 -0400

    Add operator |

--- test/bitmask_test.dart
+++ test/bitmask_test.dart
@@ -120,5 +120,20 @@ void main() {
       var mask2 = Bitmask(6);
       expect(() => mask & mask2, throwsA(isArgumentError));
     });
+
+    test('bitwise OR of two Bitmasks', () {
+      var mask = Bitmask(MaskBits.values.length);
+      mask[MaskBits.one.index] = true;
+      mask[MaskBits.three.index] = true;
+      var mask2 = Bitmask(MaskBits.values.length);
+      mask2[MaskBits.two.index] = true;
+      expect((mask | mask2).flags, 14);
+    });
+
+    test('bitwise OR of two Bitmasks that are not same size', () {
+      var mask = Bitmask(4);
+      var mask2 = Bitmask(6);
+      expect(() => mask | mask2, throwsA(isArgumentError));
+    });
   });
 }
